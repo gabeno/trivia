df39350fa4c7745e177380a01731ad958e46960a
gabeno <gmajivu@gmail.com>
2014-03-05 12:12:01 +0300

    edit writeup, refactor and housekeeping

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 ## Trivia
 
-This is a simple node cli game which I have made in the process of teaching myself programming
-
-I love JavaScript hence the bias for the node environment.
+This is a simple **node CLI** game which I have made in the process of teaching myself programming. I love JavaScript hence the bias for the node environment.
 
 ## Contibuting
 
-This s a very basic game and would definitely do with some improvements. Feel free to open an issue.
+This is a very basic game and would definitely do with some improvements. Feel free to open an issue.
 
 ## Roadmap
 
-- Improve randomization currently implemented w/ `Math.random()` function
-- Perhaps a more sophisticated `mask()` algorithm
-- Ability to set new words from the cli
+- Improve randomization which is currently implemented w/ `Math.random()` function.
+- Perhaps a more sophisticated `mask()` algorithm.
+- An easier way to set new words.
 - Intuitively set the number of chances for a player based on the difficulty of the word
-- Add a play again behaviour so that game does not end after each round.s
+- Add a _play again_ behaviour/option so that game does not end after each round.
--- index.js
+++ index.js
@@ -7,9 +7,6 @@ var guessesTaken = 0;
 var len = randomWord.word.length;
 var chances = 3;
 
-// console.log(randomWord.word);
-// console.log(randomWord.meta.hints);
-
 var readline = readline.createInterface({
   input:  process.stdin,
   output: process.stdout
@@ -45,20 +42,25 @@ readline.on('line', function(line) {
       // check nearness
       var misses = compareStr(randomWord.word, line);
       var str = misses > 1 ? 's' : '';
+
+      if (misses > Math.round(len / 2 + 1))
+        write('You are way off, I doubt you have the right word!', 1);
+
       write('You got '+ misses +' letter'+ str +' out of place. Please recheck!', 1);
     }
     readline.prompt();
 
   } else {
     write('All your chances spent!', 1)
-    write('The word is ' + chalk.green.bold(randomWord.word));
-    write('Definition: ' + chalk.blue(randomWord.meta.definition));
+    write('The word is ' + chalk.green.underline(randomWord.word));
+    write('Definition: ' + chalk.magenta(randomWord.meta.definition));
     readline.close();
   }
 
   if (line === randomWord.word) {
-    write('Congrats smart!');
-    write('Definition: ' + chalk.blue(randomWord.meta.definition));
+    write('Congrats!');
+    write('The word is ' + chalk.green.underline(randomWord.word));
+    write('Definition: ' + chalk.magenta(randomWord.meta.definition));
     readline.close();
     // readline.resume();
     // playAgain();
@@ -66,30 +68,54 @@ readline.on('line', function(line) {
   }
 });
 
+/**
+ * -------------------------------------------------------
+ *                  HELPER FUNCTIONS
+ * -------------------------------------------------------
+ */
+
 function playAgain() {
   console.log('Would you like to play another word? y/n');
   readline.pause();
 }
 
+/**
+ * mask a string by hiding some charcaters
+ * @param  {[String]} str [a string to be masked]
+ * @return {[String]}     [masked string]
+ */
 function mask(str) {
-  // add level 2 complexity
+  // add complexity
   return str.replace(/[aeiou]/g, '_');
 }
 
+/**
+ * compare two strings by letter and determine how near they compare
+ * the higher the number the lesser the two strings compare
+ * @param  {[String]} str1 [string to be compared against]
+ * @param  {[String]} str2 [string to compare]
+ * @return {[Number]}      [degree to which the strings compare]
+ */
 function compareStr(str1, str2) {
-  var counter = 0;
+  var nearness = 0;
 
   if (str1.length !== str2.length)
     return;
 
   for (var i = 0; i < str1.length; i++) {
     if (str1[i] !== str2[i])
-      counter++;
+      nearness++;
   }
 
-  return counter;
+  return nearness;
 }
 
+/**
+ * write outptut to the console
+ * @param  {[String]}   str   [string to be output]
+ * @param  {[Number]}   alert [a number to determine the level of alert]
+ * @return {[Function]}       [console.log() function]
+ */
 function write(str, alert) {
   if (alert == 1)
     return console.log(chalk.red(str));
